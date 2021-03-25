8b232aedfe36dbfc9f608aed64f2a3f511366694
Diego Giraldo <diego.dgiraldo36@gmail.com>
2021-03-25 14:03:46 -0500

    Update hiding animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,28 @@ var atcw_wc_span = document.getElementsByClassName("atcw-close")[0];
 var atcw_wc_link = document.getElementsByClassName("atcw-close-link")[0];
 
 // When the user clicks on <span> (x), close the modal
-atcw_wc_span.onclick = function() {
+atcw_wc_span.onclick = function () {
   atcw_wc_modal_close();
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-atcw_wc_link.onclick = function() {
+atcw_wc_link.onclick = function () {
   atcw_wc_modal_close();
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == atcw_wc_modal) {
     atcw_wc_modal_close();
   }
-}
+};
 
 function atcw_wc_modal_close() {
-	atcw_wc_modal.style.display = "none";
-	document.getElementsByClassName("woocommerce-notices-wrapper")[0].style.display = "none";
+  atcw_wc_modal.classList.toggle("ModalOpen");
+  atcw_wc_modal.classList.toggle("ModalClosed");
+  setTimeout(function () {
+    document.getElementsByClassName(
+      "woocommerce-notices-wrapper"
+    )[0].style.display = "none";
+  }, 400);
 }
