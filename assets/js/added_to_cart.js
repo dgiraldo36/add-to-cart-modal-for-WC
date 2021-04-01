d3c10b0fef29e76aa1db4f7aa0799e8fe5eddc34
Diego Giraldo <diego.dgiraldo36@gmail.com>
2021-04-01 17:07:41 -0500

    fix modal image

--- assets/js/added_to_cart.js
+++ assets/js/added_to_cart.js
@@ -8,11 +8,13 @@ jQuery(function ($) {
     function (e, fragments, cart_hash, $button) {
       var container = $($button).parents("li.product");
       var product_title = container.find(".woocommerce-loop-product__title").html();
-      var product_img = container.find(".attachment-woocommerce_thumbnail.size-woocommerce_thumbnail");
+      var product_img = container.find(".woocommerce-LoopProduct-link.woocommerce-loop-product__link img");
       var product_price = container.find(".price").html();
 
       $(atcw_wc_modal).find("h5 .product-title").html(product_title);
-      $(atcw_wc_modal).find(".product-item .product-img").html(product_img[0].outerHTML);
+      if (product_img.length) {
+        $(atcw_wc_modal).find(".product-item .product-img").html(product_img[0].outerHTML);
+      }
       $(atcw_wc_modal).find(".product-item .product-info .product-price-amount").html(product_price);
 
       atcw_open_modal();
